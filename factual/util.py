2ccd56d2188e28dc3ca6dfcd6f09c75ffd1e99f6
Christopher Casebeer <christopher@chc.name>
2011-07-11 12:44:35 -0700

    Reference Python IAQ regarding abstract function, remove whitespace.

--- factual/util.py
+++ factual/util.py
@@ -1,7 +1,5 @@
-
 def abstract():
+	# http://norvig.com/python-iaq.html
 	import inspect
 	caller = inspect.getouterframes(inspect.currentframe())[1][3]
 	raise NotImplementedError(caller + ' must be implemented in subclass')
-
-
