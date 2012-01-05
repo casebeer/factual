a183b5deaf099b11dc7be8bd3bc65e8b9bb12db5
Christopher H. Casebeer <christopher@chc.name>
2012-01-05 17:54:20 -0500

    Move Table class from v2/tables.py to common/util.py for use by both v2 and v3.

--- factual/common/util.py
+++ factual/common/util.py
@@ -3,3 +3,28 @@ def abstract():
 	import inspect
 	caller = inspect.getouterframes(inspect.currentframe())[1][3]
 	raise NotImplementedError(caller + ' must be implemented in subclass')
+
+
+class Table(object):
+	'''
+	Table classes hold information about Factual tables. 
+
+	At the moment, their only purpose is to hold the table's ID for the Session
+	to use in building API URLs. 
+
+	To use a new Factual table, subclass Table and set _table_id as a class variable
+	holding the new table's ID. 
+
+	'''
+	_table_id = None
+	def __init__(self, id=None):
+		self._id = id
+		# todo: query and cache schema for table?
+	def __repr__(self):
+		return self.id
+	def _get_id(self):
+		if self._id:
+			return self._id
+		else:
+			return self._table_id
+	id = property(_get_id)
--- factual/v2/tables.py
+++ factual/v2/tables.py
@@ -1,25 +1,8 @@
 '''
-Table classes hold information about Factual tables. 
-
-At the moment, their only purpose is to hold the table's ID for the Session
-to use in building API URLs. 
-
-To use a new Factual table, subclass Table and set _table_id as a class variable
-holding the new table's ID. 
-
+v2 Tables
 '''
 
-class Table(object):
-	_table_id = None
-	def __init__(self, id=None):
-		self._id = id
-		# todo: query and cache schema for table?
-	def _get_id(self):
-		if self._id:
-			return self._id
-		else:
-			return self._table_id
-	id = property(_get_id)
+from factual.common.util import Table
 
 class USPOI(Table):
 	'''
